7d4dac3c3a3255e6a616d6dd589dd1e6fc282645
unknown <you@example.com>
2022-01-20 16:29:43 -0300

    first commit

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,19 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.generation.blogPessoal.model.Postagem; 
+
+@Repository
+public interface  PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+	
+}
